8206554ed9ae2bcf7390c76ced443197da771331
Daniel Ng <dvng13@gmail.com>
2015-04-30 12:15:08 -0700

    Finish slide moves and start hop moves

--- Week_2/w2d4/checkers/board.rb
+++ Week_2/w2d4/checkers/board.rb
@@ -15,17 +15,25 @@ class Board
   end
 
   def place_piece(piece, pos)
-    self.grid[pos.first][pos.last] = piece
+    grid[pos.first][pos.last] = piece
   end
 
   def empty?(pos)
-    self.grid[pos.first][pos.last].nil?
+    grid[pos.first][pos.last].nil?
+  end
+
+  def find_piece(pos)
+     grid[pos.first][pos.last]
+  end
+
+  def self.on_board?(pos)
+    pos.all? { |ele| ele.between?(0,7) }
   end
 
   def display
     # print "    A   B   C   D   E   F   G   H\n"
     print "    0    1    2    3    4    5    6    7\n"
-    @grid.each_with_index do |row, row_index|
+    grid.each_with_index do |row, row_index|
       print "#{row_index}   "
       row.each do |col|
         if col.nil?
@@ -59,7 +67,34 @@ class Board
     end
   end
 
+  def move(start_pos, end_pos)
+    current = find_piece(start_pos)
+    place_piece(current, end_pos)
+  end
+
+  def delete(pos)
+    grid[pos.first][pos.last] = nil
+  end
+
 end
 
 board = Board.new
 board.display
+
+board.move([6,1], [2,1])
+board.delete([6,1])
+
+board.move([1,3],[2,3])
+board.delete([1,3])
+board.display
+
+
+current = board.find_piece([1,2])
+p current.hop_moves
+
+# current = board.find_piece([1,1])
+# current.perform_slide([2,0])
+# current.perform_slide([3,1])
+# current.perform_slide([4,0])
+# current.perform_slide([5,1])
+# current.perform_slide([6,1])
--- Week_2/w2d4/checkers/piece.rb
+++ Week_2/w2d4/checkers/piece.rb
@@ -1,26 +1,79 @@
 class Piece
 
-  def initialize(position, color, board)
-    @position, @color, @board = position, color, board
+  attr_reader :pos, :color, :king
+
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @king = false
-    @board.place_piece(self, @position)
+    @board.place_piece(self, @pos)
   end
 
-  FORWARD_DIR = [
+  UP_DIR = [
     [-1,-1],
     [-1, 1]
   ]
 
-  BACKWARD_DIR = [
+  DOWN_DIR = [
     [1,-1],
     [1, 1]
   ]
 
+  def move_diffs
+    return UP_DIR.concat(DOWN_DIR) if kinged?
+    case @color
+    when :white
+      DOWN_DIR
+    when :black
+      UP_DIR
+    end
+  end
+
+  def slide_moves
+    move_diffs.map do |diff|
+      # puts "pos: #{pos} : diff: #{diff} : added: #{[diff.first + pos.first, diff.last + pos.first]}"
+      [diff.first + pos.first, diff.last + pos.last]
+    end.reject do |spot|
+      !@board.empty?(spot)
+    end.select do |spot|
+      Board.on_board?(spot)
+    end
+  end
+
+  def hop_moves
+    hoppable_opponent = move_diffs.map do |diff|
+      # puts "pos: #{pos} : diff: #{diff} : added: #{[diff.first + pos.first, diff.last + pos.first]}"
+      [diff.first + pos.first, diff.last + pos.last]
+    end.reject do |pos|
+      @board.empty?(pos)
+    end.reject do |pos|
+      @board.find_piece(pos).color == color
+    end
+
+    hoppable_opponent.map { |start|
+      diff = [start.first - pos.first, start.last - pos.last]
+      [start.first + diff.first, start.last + diff.last]
+    }
+  end
+
+  def perform_slide(end_pos)
+     if slide_moves.include?(end_pos)
+       @board.move(pos, end_pos)
+       @board.grid[pos.first][pos.last] = nil
+       @pos = end_pos
+     else
+       raise "Not a valid slide move"
+     end
+  end
+
+  def perform_jump(end_pos)
+
+  end
+
   def render_piece
     if @color == :white
-      print "\u2617"
+      print kinged? ? "\u265B" : "\u2617"
     elsif @color == :red
-      print "\u2616"
+      print kinged? ? "\u2655" : "\u2616"
     end
   end
 
