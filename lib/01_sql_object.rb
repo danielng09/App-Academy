a4c8767ef65de83257e7d25905ee25662970bb8c
Daniel Ng <dvng13@gmail.com>
2015-05-08 14:06:18 -0700

    Finish Phase 1b - 1e

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,10 +5,26 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    results = DBConnection.execute2(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{table_name};
+    SQL
+
+    results.first.map { |col| col.to_sym }
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method("#{col}=") do |value|
+        attributes[col] = value
+      end
+
+      define_method("#{col}") do
+        attributes[col]
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -20,23 +36,36 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      #{table_name};
+    SQL
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |instance|
+      thing = self.new(instance)
+    end
   end
 
   def self.find(id)
-    # ...
+    
   end
 
   def initialize(params = {})
-    # ...
+    cols = self.class.columns
+    params.each do |key, value|
+      att_name = key.to_sym
+      raise "unknown attribute #{key}" unless cols.include?(att_name)
+      self.send("#{key}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
