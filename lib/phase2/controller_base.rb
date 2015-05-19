c317df300aa0f39a7fff96589b9b94d61243a5a7
Daniel Ng <dvng13@gmail.com>
2015-05-19 15:04:00 -0700

    Create basic ControllerBase

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -4,20 +4,32 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
+      @already_built_response = false
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "can't render twice" if already_built_response?
+      @already_built_response = true
+      @res["location"] = url
+      @res.status = 302
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, content_type)
+      raise "can't render twice" if already_built_response?
+      @already_built_response = true
+      @res.body = content
+      @res.content_type = content_type
     end
   end
 end
